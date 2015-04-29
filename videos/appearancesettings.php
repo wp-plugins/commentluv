dea84a9981a449b9bc02dd410dd2e6bc55c27511
commentluv <commentluv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 07:52:38 +0000

    tagging 2.94
    git-svn-id: https://plugins.svn.wordpress.org/commentluv/tags/2.94@1148772 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ videos/appearancesettings.php
@@ -0,0 +1,3 @@
+<?php
+?>
+<iframe width="560" height="315" src="//www.youtube.com/embed/nBXkC4iHynY" frameborder="0" allowfullscreen></iframe>
---
+++ videos/operationalsettings.php
@@ -0,0 +1,3 @@
+<?php
+?>
+<iframe width="560" height="315" src="//www.youtube.com/embed/GAZgxqw6fQM" frameborder="0" allowfullscreen></iframe>
